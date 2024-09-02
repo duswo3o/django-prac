2d39db3dbbe9a80f1ac6d8c5b51ca5a5bd43bc7c
yeonjae <pyj3740@gmail.com>
2024-09-02 16:28:18 +0900

    update allowed hosts

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -29,7 +29,11 @@ OPENAI_API_KEY = config.OPENAI_API_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "3.36.49.175",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
